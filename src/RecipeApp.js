921c4e5c915e937a468dd9b3fce91fe2e86bbff3
Jacob Richardson <jrichardson@paviliongift.com>
2019-04-15 12:57:52 -0400

    Basic Recipe React App.
    This commit finishes out the Recipe app by implementing the neccessary classses and using prop-types to enforce prop types.

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 import './RecipeApp.css';
-import Recipe from './Recipe';
+import RecipeList from './RecipeList';
+import Navbar from './Navbar';
 
 class RecipeApp extends Component {
+
+
+
   render() {
     return (
       <div className="App">
-        <Recipe title="pasta" ingredients={['flour', 'water']}/>
+        <Navbar />
+        <RecipeList />
       </div>
     );
   }
